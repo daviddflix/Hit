5d71dfc5917e160ef9eebd967770a3964bafffe7
daviddflix <david-972010@hotmail.com>
2022-06-09 14:27:41 -0300

    heigth

--- src/Component/resumenCarrito/styles.js
+++ src/Component/resumenCarrito/styles.js
@@ -4,7 +4,7 @@ import {RiArrowLeftSLine} from 'react-icons/ri'
 
 export const MainContainer = styled.div`
 @media screen and (max-width: 900px){
- min-height: 70vh;
+
  display: flex;
  flex-direction: column;
  justify-content: space-around;
